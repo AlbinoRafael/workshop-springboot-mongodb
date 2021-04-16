bfdf2efa55a49831be5ad3d640d8a5e03cd3b494
Rafael Albino <fael10pg@gmail.com>
2021-04-16 14:23:17 -0300

    Consulta simples com query methods

--- src/main/java/com/casa/workshopmongo/config/Instantiation.java
+++ src/main/java/com/casa/workshopmongo/config/Instantiation.java
@@ -45,13 +45,13 @@ public class Instantiation implements CommandLineRunner {
 		CommentDTO c1 = new CommentDTO("boa viagem mano", sdf.parse("21/03/2018"), new AuthorDTO(alex));
 		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
 		CommentDTO c3 = new CommentDTO("tenha um otimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
-		
-		post1.getComments().addAll(Arrays.asList(c1,c2));
+
+		post1.getComments().addAll(Arrays.asList(c1, c2));
 		post2.getComments().addAll(Arrays.asList(c3));
-			
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
-		
-		maria.getPosts().addAll(Arrays.asList(post1,post2));
+
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
 		userRepository.save(maria);
 	}
 }
